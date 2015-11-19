bb0e2c82c9aec1d46a945a162af3b67086b33979
PaddyQuinn <padraicquinn20@gmail.com>
2015-11-18 19:15:26 -0500

    added event listeners

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-	$("#new")[0].addEventListener("click", function(event) {});
+	$("#new")[0].addEventListener("click", function(event) {
+		console.log("new");
+	});
+	$("#user")[0].addEventListener("click", function(event) {
+		console.log("user");
+	});
+	$("#media")[0].addEventListener("click", function(event) {
+		console.log("media");
+	});
 });
