0b1b348c92b2d4363298d19b0e304174df607804
PaddyQuinn <padraicquinn20@gmail.com>
2015-11-23 15:34:23 -0500

    completed functionality for adding and deleting groups

--- script.js
+++ script.js
@@ -183,28 +183,78 @@ var searchOnCoordinates = function(data) {
 		cell.innerHTML = "No results.";
 	}
 };
-var createTable = function() {
+var createGroup = function() {
 	var table = $("#groups")[0];
-	console.log($("#groups"));
 	var rows = table.rows;
 	var rowIndex = rows.length - 1;
 	var row = rows[rowIndex];
 	var cellIndex = row.cells.length - 1;
 	var cell;
 	if (cellIndex == 4) { // add a new row after 5 cells 
-		rowIndex++;
-		row = table.insertRow(rowIndex);
+		row = table.insertRow(++rowIndex);
 		cellIndex = 0;
 		cell = row.insertCell(cellIndex);
 		cell.innerHTML = row.previousElementSibling.cells[4].innerHTML;
-		row.previousElementSibling.cells[4].innerHTML = "group";
+		row.previousElementSibling.cells[4].innerHTML = "<button id=\"" + ((rowIndex - 1) * 5 + 4) + "\"onclick=\"deleteGroup(this.id)\">Delete Group</button>";
 	} else {
-		cellIndex++;
-		cell = row.insertCell(cellIndex);
+		cell = row.insertCell(++cellIndex);
 		cell.innerHTML = cell.previousElementSibling.innerHTML;
-		cell.previousElementSibling.innerHTML = "group";
+		cell.previousElementSibling.innerHTML = "<button id=\"" + (rowIndex * 5 + cellIndex - 1) + "\"onclick=\"deleteGroup(this.id)\">Delete Group</button>";
 	}
 };
+
+var deleteGroup = function(id) {
+	console.log($("#groups"));
+	var rowIndex = Math.floor(id / 5);
+	var cellIndex = id % 5;
+	var table = $("#groups")[0];
+	console.log(table);
+	var rows = table.rows;
+	var row;
+	var nextRow;
+	var cells;
+	var cell;
+	var nextCell;
+	var button;
+	while (rowIndex < rows.length) { // loop through all rows
+		row = rows[rowIndex++]
+		cells = row.cells;
+		while (cellIndex < cells.length) { // loop through all cells in a given row
+			cell = cells[cellIndex];
+			nextCell = cell.nextElementSibling;
+			if (nextCell != null) { // overwrite cell with next cell if it is not the last of its row
+				cell.innerHTML = nextCell.innerHTML;
+				button = cell.children[0].id;
+				if (button != "") { // avoid unwanted side effect of assigning id to new group button
+					button.id--;
+				}
+			} else { // handle cells that are the last of its row
+				if (cellIndex < 4) { // delete cells that do not have a next row
+					row.deleteCell(cellIndex);
+				} else {
+					nextRow = row.nextElementSibling;
+					if (nextRow != null) { // overwrite cell with next cell if it has a next row
+						cell.innerHTML = nextRow.cells[0].innerHTML;
+						button = cell.children[0].id;
+						if (button != "") { // avoid unwanted side effect of assigning id to new group button
+							button.id--;
+						}
+					} else { // otherwise just delete cell
+						row.deleteCell(cellIndex);
+					}
+				}
+			}
+			cellIndex++;
+		}
+		cellIndex = 0;
+	}
+	var lastRowIndex = rows.length - 1;
+	console.log(lastRowIndex);
+	if (rows[lastRowIndex].cells.length == 0) {
+		table.deleteRow(lastRowIndex);
+	}
+	console.log(rows);
+}
 
 var search = function() {
 	$("#groupspage")[0].hidden = true;
