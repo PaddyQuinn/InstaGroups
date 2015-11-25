9fc45ec2f6c02964ea88fa9cf837c01d0402ecfb
PaddyQuinn <padraicquinn20@gmail.com>
2015-11-25 04:23:16 -0500

    app is nearly fully functional

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var access_token = "20203233.9e4190f.72c45bfbc5d14f24aecf3d2d85af78e3";
 
 var goHome = function() {
-	$("#searchMessage")[0].innerHTML = "";
+	//$("#searchMessage")[0].innerHTML = "";
 	$("#grouppage")[0].hidden = true;
 	$("#userpage")[0].hidden = true;
 	$("#searchpage")[0].hidden = true;
@@ -9,8 +9,8 @@ var goHome = function() {
 };
 
 var group = function() {
-	$("#groupMessage")[0].innerHTML = "";
-	$("#searchMessage")[0].innerHTML = "";
+	//$("#homeMessage")[0].innerHTML = "";
+	//$("#searchMessage")[0].innerHTML = "";
 	$("#homepage")[0].hidden = true;
 	$("#userpage")[0].hidden = true;
 	$("#searchpage")[0].hidden = true;
@@ -18,8 +18,8 @@ var group = function() {
 };
 
 var user = function() {
-	$("#groupMessage")[0].innerHTML = "";
-	$("#searchMessage")[0].innerHTML = "";
+	//$("#homeMessage")[0].innerHTML = "";
+	//$("#searchMessage")[0].innerHTML = "";
 	$("#homepage")[0].hidden = true;
 	$("#grouppage")[0].hidden = true;
 	$("#searchpage")[0].hidden = true;
@@ -27,77 +27,131 @@ var user = function() {
 };
 
 var search = function() {
-	$("#groupMessage")[0].innerHTML = "";
-	$("#searchMessage")[0].innerHTML = "";
+	//$("#homeMessage")[0].innerHTML = "";
+	//$("#searchMessage")[0].innerHTML = "";
 	$("#homepage")[0].hidden = true;
 	$("#grouppage")[0].hidden = true;
 	$("#userpage")[0].hidden = true;
 	$("#searchpage")[0].hidden = false;
 };
 
-var createGroup = function(element) {
-	$("#groupMessage")[0].innerHTML = "";
-	var table = $("#groups")[0];
-	var rows = table.rows;
-	var rowIndex = rows.length - 1;
-	var row = rows[rowIndex];
-	var cellIndex = row.cells.length - 1;
-	var cell;
-	var id;
-	var groupName;
-	if (cellIndex == 4) { // add a new row after 5 cells 
-		row = table.insertRow(++rowIndex);
-		cellIndex = 0;
-		cell = row.insertCell(cellIndex);
-		cell.innerHTML = row.previousElementSibling.cells[4].innerHTML;
-		id = (rowIndex - 1) * 5 + 4;
-		groupName = checkGroupName(id);
-		row.previousElementSibling.cells[4].innerHTML = "<div id=\"" + 
+var createGroup = function(pic, username, id) { 
+	//$("#homeMessage")[0].innerHTML = "";
+	$("#grouppage")[0].children[1].children[1].innerHTML = "";
+	var textbox = "<form onsubmit=\"editGroup()\"><input type=\"text\" id=\"group\"></input><input type=\"text\" id=\"p\" value=\"" +
+				  pic +
+				  "\" hidden></input><input type=\"text\" id=\"u\" value=\"" +
+				  username +
+				  "\" hidden></input><input type=\"text\" id=\"i\" value=\"" +
+				  id +
+				  "\" hidden></input><input type=\"text\" id=\"exists\" value=\"false\" hidden></input><input type=\"submit\" value=\"Submit\"></input></form>";
+	if (id != -1) {
+		var row = $("#groupContents")[0].insertRow(0);
+		var cell = row.insertCell(0);
+		cell.innerHTML = "<img src=\"" + 
+					  	 pic + 
+						 "\"><br><a href=\"https://www.instagram.com/" + 
+						 username +
+				  	  	 "\">" + 
+					  	 username + 
+						 "</a><button onclick=\"getRecent(" + 
+						 id + 
+						 ")\">Recent Uploads</button><button onclick=\"removeFromGroup(this)\">Remove</button>";
+	}
+	$("#grouppage")[0].children[1].children[0].outerHTML = textbox;
+	group();
+	$("#group")[0].focus();
+};
+
+var editGroup = function() {
+	event.preventDefault();
+	var groupName = $("#group")[0].value;
+	var pic = $("#p")[0].value;
+	var username = $("#u")[0].value;
+	var id = $("#i")[0].value;
+	var content = "";
+	if (id != -1) {
+		content += "<li class=\"" + 
+				   username + 
+				   "\" hidden>" +
+				   pic +
+				   "</li><li class=\"" +
+				   username + 
+				   "\">" + 
+				   username + 
+				   "</li><li class=\"" + 
+				   username + 
+				   "\" hidden>" + 
+				   id +
+				   "</li>";
+	}
+	var exists = $("#exists")[0].value;
+	var message = checkGroupName(groupName);
+	if (message == groupName) {
+		$("#grouppage")[0].children[1].children[0].outerHTML = "<h1 id=\"currentGroup\">" + groupName + "</h1>"; //EDIT ONCLICK??
+		if (exists == "false") {
+			var table = $("#groups")[0];
+			var rows = table.rows;
+			var rowIndex = rows.length - 1;
+			var row = rows[rowIndex];
+			var cellIndex = row.cells.length - 1;
+			var cell;
+			if (cellIndex == 4) { // add a new row after 5 cells 
+				row = table.insertRow(++rowIndex);
+				cellIndex = 0;
+				cell = row.insertCell(cellIndex);
+				cell.innerHTML = row.previousElementSibling.cells[4].innerHTML;
+				row.previousElementSibling.cells[4].innerHTML = "<div id=\"" + 
+																groupName + 
+																"\" class=\"groupName\">" + 
+																groupName + 
+																"<ul>" +
+																content +
+																"</ul></div><button onclick=\"viewGroup(this)\">View/Edit</button><button onclick=\"deleteGroup(this)\">Delete Group</button>";
+				addToMenus(groupName);
+			} else {
+				cell = row.insertCell(++cellIndex);
+				cell.innerHTML = cell.previousElementSibling.innerHTML;
+				cell.previousElementSibling.innerHTML = "<div id=\"" + 
 														groupName + 
 														"\" class=\"groupName\">" + 
 														groupName + 
 														"<ul>" +
-														element +
+														content + 
 														"</ul></div><button onclick=\"viewGroup(this)\">View/Edit</button><button onclick=\"deleteGroup(this)\">Delete Group</button>";
-		addToMenus(groupName);
+				addToMenus(groupName);
+			}
+			message = groupName + " successfully created.";
+			if (id != -1) {
+				message += username + " successfully added to " + groupName + ".";
+			}
+			//$("#homeMessage")[0].innerHTML = message;
+			//window.scrollTo(0, 0);
+		} else {
+			//TODO
+		}
 	} else {
-		cell = row.insertCell(++cellIndex);
-		cell.innerHTML = cell.previousElementSibling.innerHTML;
-		id = rowIndex * 5 + cellIndex - 1;
-		groupName = checkGroupName(id);
-		cell.previousElementSibling.innerHTML = "<div id=\"" + 
-												groupName + 
-												"\" class=\"groupName\">" + 
-												groupName + 
-												"<ul>" +
-												element +
-												"</ul></div><button onclick=\"viewGroup(this)\">View/Edit</button><button onclick=\"deleteGroup(this)\">Delete Group</button>";
-		addToMenus(groupName);
+		//$("#groupMessage")[0].innerHTML = message;
 	}
-	var message = groupName + " successfully created.";
-	if (element != "") {
-		message += " successfully added to " + groupName + ".";
-	}
-	$("#groupMessage")[0].innerHTML = message;
-	window.scrollTo(0, 0);
-	console.log($("html")[0]);
 };
 
 var checkGroupName = function(groupName) {
 	//groupName = groupName.replace(/ /g, "+"); ENFORCE UNIQUE GROUP NAMES
 	//check for "New Group"
-	if ($("#" + groupName).length > 0) {
-		return checkGroupName(groupName + "-1");
+	if (groupName == "") {
+		return "Please enter a group name."
+	} else if ($("#" + groupName).length > 0) {
+		return groupName + " already used.";
 	} else {
 		return groupName;
 	}
 };
 
-var viewGroup = function(button) {
-	$("#groupMessage")[0].innerHTML = "";
+var viewGroup = function(button) {//TO EDIT
+	//$("#homeMessage")[0].innerHTML = "";
 	var div = button.parentElement.children[0];
 	var groupName = div.id;
-	$("#group")[0].innerHTML = groupName;
+	$("#grouppage")[0].children[1].children[0].outerHTML = "<h1 id=\"currentGroup\">" + groupName + "</h1>"; //EDIT ONCLICK??
 	var list = div.children[0].children;
 	var table = $("#groupContents")[0];
 	table.innerHTML = "";
@@ -131,6 +185,74 @@ var viewGroup = function(button) {
 	group();
 };
 
+var getRecent = function(id) { //PRIVATE USERS???
+	$.ajax({url: "https://api.instagram.com/v1/users/" + id + "/media/recent/?access_token=" + access_token,
+			dataType: "jsonp",
+			success: handleMedia				
+	});
+	//ON READY STATE??
+	user();
+};
+
+var handleMedia = function(data) {
+	var table;
+	if (this.url.indexOf("https://api.instagram.com/v1/users/") < 0) {
+		table = $("#searchResults")[0];
+	} else {
+		table = $("#userResults")[0];
+		$("#user")[0].innerHTML = data.data[0].user.username;
+	}
+	table.innerHTML = "";
+	var row;
+	var cell;
+	// make sure correct data is returned
+	if (data.meta.code != 200) {
+		table.innerHTML = data.meta.error_message;
+	} else {
+		var media = data.data;
+		var content = "";
+		// handle case when no data is returned
+		if (media.length < 1) {
+			table.innerHTML = "No results."
+		} else {
+			var index = 0;
+			var pv;
+			while (index < media.length) {
+				pv = media[index];
+				if (pv.type == "image") {
+					content += "<img src=\"" +
+				    pv.images.standard_resolution.url +
+			  	    "\">";
+				} else if (pv.type == "video") { // handles videos
+					content += "<video controls><source src=\"" +
+			   	    pv.videos.standard_resolution.url +
+				    "\" type=\"video/mp4\">Your browser does not support the video tag</video>";
+				}
+				//ADD POSTING USER -> SO THEY CAN BE ADDED TO GROUP (REQUIRED)
+				if (pv.caption != null) {
+					content += "<br>Caption: " + pv.caption.text;
+				} //UI DECISION NOT TO DISPLAY ANYTHING WHEN THERE IS NO CAPTION
+				content += "<br>NumLikes: " +
+				   		   pv.likes.count +
+						   "<br>Time: " +
+						   convert(pv.created_time) +
+						   "<br><a href=\"" +
+						   pv.link +
+						   "\">Instagram</a>";
+				//UI DECISION TO NOT HAVE A SEPARATE THING FOR TAGS!!!!COMMENTS CAN RESULT IN A PIC BEING TAGGED - RETHINK DECISION???
+				row = table.insertRow(index++);
+				cell = row.insertCell(0);
+				cell.innerHTML = content;
+				content = "";
+			}
+		}
+	}
+};
+
+var convert = function(millis) { //TODO!!!
+	return millis;
+};
+
 var removeFromGroup = function(button) {
 	var cell = button.parentElement;
 	var username = cell.children[2].innerHTML;
@@ -160,8 +282,8 @@ var checkNextRow = function(cell, nextCell) {
 	return nextCell;
 };
 
-var deleteGroup = function(button) {
-	$("#groupMessage")[0].innerHTML = "";
+var deleteGroup = function(button) { //MUST HANDLE WHEN DELETED GROUP IS ON VIEW GROUP PAGE
+	//$("#homeMessage")[0].innerHTML = "";
 	var cell = button.parentElement;
 	var groupName = cell.children[0].id;
 	$("." + groupName).remove();
@@ -179,8 +301,8 @@ var deleteGroup = function(button) {
 		row.remove();
 	}
 	console.log($("html")[0]);
-	$("#groupMessage")[0].innerHTML = groupName + " successfully deleted.";
-	window.scrollTo(0, 0);
+	//$("#homeMessage")[0].innerHTML = groupName + " successfully deleted.";
+	//window.scrollTo(0, 0);
 };
 
 var addToMenus = function(groupName) {
@@ -309,15 +431,6 @@ var getGroups = function() {
 	return ret;
 };
 
-var getRecent = function(id) { //PRIVATE USERS???
-	$.ajax({url: "https://api.instagram.com/v1/users/" + id + "/media/recent/?access_token=" + access_token,
-			dataType: "jsonp",
-			success: handleMedia				
-	});
-	//ON READY STATE??
-	user();
-};
-
 var addToGroup = function(select, pic, username, id) {
 	var groupName = select.value;
 	var element = "<li class=\"" + 
@@ -334,9 +447,9 @@ var addToGroup = function(select, pic, username, id) {
 				  id +
 				  "</li>";
 	if (groupName != "New Group") {
-		if ($("#" + groupName + " ." + username).length < 1) { // check if user is already in group
+		if ($("#" + groupName + " ." + username).length < 1) { // if user is not in group
 			$("#" + groupName)[0].children[0].innerHTML += element;
-			var header = $("#group")[0];
+			var header = $("#currentGroup")[0]; //WHAT IF NO GROUP BEING DISPLAYED
 			if (header.innerHTML == groupName) {
 				var content = "<img src=\"" + 
 					  		  pic + 
@@ -374,70 +487,11 @@ var addToGroup = function(select, pic, username, id) {
 			$("#searchMessage")[0].innerHTML = username + " already belongs to " + groupName;
 		}
 		window.scrollTo(0, 0);
-	} else {
-		createGroup(element);
-		goHome();
+	} else { //CHECK FUNCTIONALITY
+		createGroup(pic, username, id);
+		group();
 	}
 	select[0].selected = true;
-};
-
-var handleMedia = function(data) {
-	var table;
-	if (this.url.indexOf("https://api.instagram.com/v1/users/") < 0) {
-		table = $("#searchResults")[0];
-	} else {
-		table = $("#userResults")[0];
-		$("#user")[0].innerHTML = data.data[0].user.username;
-	}
-	table.innerHTML = "";
-	var row;
-	var cell;
-	// make sure correct data is returned
-	if (data.meta.code != 200) {
-		table.innerHTML = data.meta.error_message;
-	} else {
-		var media = data.data;
-		var content = "";
-		// handle case when no data is returned
-		if (media.length < 1) {
-			table.innerHTML = "No results."
-		} else {
-			var index = 0;
-			var pv;
-			while (index < media.length) {
-				pv = media[index];
-				if (pv.type == "image") {
-					content += "<img src=\"" +
-				    pv.images.standard_resolution.url +
-			  	    "\">";
-				} else if (pv.type == "video") { // handles videos
-					content += "<video controls><source src=\"" +
-			   	    pv.videos.standard_resolution.url +
-				    "\" type=\"video/mp4\">Your browser does not support the video tag</video>";
-				}
-				//ADD POSTING USER -> SO THEY CAN BE ADDED TO GROUP (REQUIRED)
-				if (pv.caption != null) {
-					content += "<br>Caption: " + pv.caption.text;
-				} //UI DECISION NOT TO DISPLAY ANYTHING WHEN THERE IS NO CAPTION
-				content += "<br>NumLikes: " +
-				   		   pv.likes.count +
-						   "<br>Time: " +
-						   convert(pv.created_time) +
-						   "<br><a href=\"" +
-						   pv.link +
-						   "\">Instagram</a>";
-				//UI DECISION TO NOT HAVE A SEPARATE THING FOR TAGS!!!!COMMENTS CAN RESULT IN A PIC BEING TAGGED - RETHINK DECISION???
-				row = table.insertRow(index++);
-				cell = row.insertCell(0);
-				cell.innerHTML = content;
-				content = "";
-			}
-		}
-	}
-};
-
-var convert = function(millis) { //TODO!!!
-	return millis;
 };
 
 var searchOnCoordinates = function(data) {
